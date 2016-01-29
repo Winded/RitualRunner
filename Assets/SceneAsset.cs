c76ce522233d9370c87691d360edc99ed7f41b78
Vuorinen Atte TTV15SP <attevuorinen@kamk.fi>
2016-01-29 21:33:51 +0200

    SceneAsset and SceneCreator scripts

---
+++ Assets/SceneAsset.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+
+
+
+//============================//////////////
+//||         TETRIS!        ||//////////////
+//============================//////////////
+//||                        ||//-=======-///
+//||                        ||//-|SCORE|-///
+//||          ######        ||//-|00100|-///
+//||          ##            ||//-=======-///
+//||                        ||//////////////
+//||                        ||//-========-//
+//||                        ||//-  NEXT  -//
+//||                        ||//-========-//
+//||                        ||//-        -// 
+//||                        ||//-  ####  -//
+//||                        ||//-  ####  -//
+//||                        ||//-        -//
+//||                        ||//-========-//
+//||                        ||//////////////
+//||                ##      ||//////////////
+//||    ##      ||||##  ####||//////////////
+//||  ######  ||||####  ####||//////////////
+//============================//////////////
+
+
+
+
+
+public class SceneAsset : ScriptableObject 
+{
+	#if UNITY_EDITOR
+	[MenuItem("Assets/Create/SceneAsset")]
+	static void CreateSceneAsset()
+	{
+		SceneAsset asset = ScriptableObject.CreateInstance<SceneAsset> ();
+
+		AssetDatabase.CreateAsset (asset, "Assets/MyScene.asset");
+	}
+	#endif
+
+	public enum SceneObjectType
+	{
+		Block,
+		Obstacle,
+		Hole,
+		Enemy,
+		Custom,
+	}
+
+	public enum SceneObjectPropertyType
+	{
+		None,
+		Scale,
+		Offset,
+	}
+
+	[System.Serializable]
+	public struct SceneObjectProperty
+	{
+		public SceneObjectPropertyType type;
+		public Vector2 data;
+	}
+
+	[System.Serializable]
+	public class DataObject
+	{
+		public int time;
+		public int range;
+
+		public SceneObjectType type;
+		public int customID;
+
+		public SceneObjectProperty[] properties;
+	}
+
+	[System.Serializable]
+	public struct SceneObject
+	{ 
+		public GameObject prefab;
+		public SceneObjectType type;
+		
+		public int customID;
+	}
+
+	public SceneObject[] sceneObjects;
+	public DataObject[] dataObjects;
+}
---
+++ Assets/SceneCreator.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneCreator : MonoBehaviour 
+{
+
+#if UNITY_EDITOR
+	[UnityEditor.Callbacks.PostProcessScene]
+	private static void EditorLoadScene()
+	{
+		SceneCreator sceneCreator = GameObject.FindObjectOfType<SceneCreator> ();
+
+		if (sceneCreator) 
+		{
+			sceneCreator.LoadScene();
+		}
+	}
+#endif
+
+	public float randomOffet = 0.03f;
+
+	public int scale = 10;
+
+	public int lenght = 60;
+	public SceneAsset asset;
+		
+	private void LoadScene()
+	{
+		Vector2 position = transform.position;
+		int time = 0;
+
+		while (time < lenght) 
+		{
+			time++;
+			
+			GameObject prefab = null;
+
+			SceneAsset.DataObject obj = null;
+
+			for (int i = 0; i < asset.dataObjects.Length; i++) 
+			{
+				obj = asset.dataObjects[i];
+
+				if( time >= obj.time && time <= obj.time + obj.range)
+				{
+					prefab = FindObject(obj.type, obj.customID);
+					break;
+				}
+				else
+				{
+					prefab = FindObject(SceneAsset.SceneObjectType.Block, obj.customID);
+				}
+			}
+			
+			if(prefab != null)
+			{
+				for(int i = 0; i < scale; i++)
+				{ 
+					//Debug.Log(prefab.name + " " + scale + ": " + time);
+					GameObject gobj = GameObject.Instantiate(prefab, position, Quaternion.identity) as GameObject;
+
+					gobj.transform.parent = transform;
+
+					SpriteRenderer sprite = gobj.GetComponent<SpriteRenderer>();
+
+					float offset = gobj.transform.localScale.x;
+
+					if(sprite != null)
+					{
+						offset = sprite.bounds.size.x;
+					}
+
+					if(obj != null)
+					{
+						for(int j = 0; j < obj.properties.Length; j++)
+						{
+							if(obj.properties[j].type == SceneAsset.SceneObjectPropertyType.Scale)
+							{			
+								gobj.transform.localScale += new Vector3(obj.properties[j].data.x, obj.properties[j].data.y);
+
+								float oldOffset = offset;
+								offset = gobj.transform.localScale.x;
+								
+								if(sprite != null)
+								{
+									offset = sprite.bounds.size.x;
+								}
+
+								gobj.transform.Translate(oldOffset,0,0);
+							}
+							else if(obj.properties[j].type == SceneAsset.SceneObjectPropertyType.Offset)
+							{
+								gobj.transform.Translate(obj.properties[j].data.x, obj.properties[j].data.y, 0);
+							}
+						}
+					}
+
+					position.x += offset;
+
+					if(Random.Range(0,100) > 50)
+					{
+						position.y += Random.Range(-randomOffet,randomOffet);
+					}
+				}
+
+				for(int i = 0; i < obj.properties.Length; i++)
+				{
+					if(obj.properties[i].type == SceneAsset.SceneObjectPropertyType.Offset) 
+					{
+						position += obj.properties[i].data;
+					}
+				}
+			}
+		}
+	}
+
+	private GameObject FindObject(SceneAsset.SceneObjectType type, int customID)
+	{
+		for(int i = 0; i < asset.sceneObjects.Length; i++)
+		{
+			if(asset.sceneObjects[i].type == type || (type == SceneAsset.SceneObjectType.Custom && asset.sceneObjects[i].customID == customID))
+			{
+				return asset.sceneObjects[i].prefab;
+			}
+		}
+
+		return null;
+	}
+}
