d17ddc9a9727dd29663bbd6bc20f85bedc8d45cb
Antton Hytönen <hytonen-a@hotmail.com>
2016-01-30 21:12:27 +0200

    Moving background n stuff

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+	public Vector3 Movement = Vector3.left;
+
+	private bool mMoving = true;
+
+	void Start() {
+		Game.State.OnStateChanged += StateChanged;
+	}
+
+	void Update() {
+		if (mMoving) {
+			var pos = transform.localPosition + Movement * Time.deltaTime;
+			transform.localPosition = pos;
+		}
+	}
+
+	void StateChanged (GameStateEnum oldState, GameStateEnum newState) {
+		if (newState == GameStateEnum.GameOver) {
+			mMoving = false;
+		}
+	}
+}
