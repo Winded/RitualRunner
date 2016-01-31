20347c83ee98c125edfdc9b95ea1b4213143ae72
Antton Hytönen <hytonen-a@hotmail.com>
2016-01-31 11:25:21 +0200

    yayayayayayay

---
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	public void PlayTutorial() {
+		Application.LoadLevel (1);
+	}
+	
+	public void PlayFirebrand() {
+		Application.LoadLevel (2);
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+}
